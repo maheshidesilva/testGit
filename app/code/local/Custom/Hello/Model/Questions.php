d3e4a0072773e77c1862985189a5f216cf3b485d
maheshidesilva <maheshide.silva@gmail.com>
2014-06-26 21:30:19 +1000

    my first commit

--- app/code/local/Custom/Hello/Model/Questions.php
+++ app/code/local/Custom/Hello/Model/Questions.php
@@ -4,6 +4,7 @@ class Custom_Hello_Model_Questions extends Mage_Core_Model_Abstract
 {
     protected function _construct()
     {
+        //this is my test git comment
         $this->_init('hello/questions');
     }   
 }
